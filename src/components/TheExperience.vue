b03fd862c9b4faecf3735327b5473b8c03183ddb
alvarosabu <alvaro.saburido@gmail.com>
2023-03-12 10:17:05 +0100

    feat: update deps and simplify example

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -17,24 +17,16 @@ const state = reactive({
 <template>
   <TresCanvas v-bind="state">
     <OrbitControls />
-    <TresPerspectiveCamera :position="[7, 7, 7]" :look-at="[0, 4, 0]" />
+    <TresPerspectiveCamera :position="[7, 7, 7]" :look-at="[0, 0, 0]" />
     <TresScene>
-      <TresGroup :position="[0, -4, 0]">
-        <TresMesh :position="[-2, 6, 0]" :rotation="[0, Math.PI, 0]" cast-shadow>
-          <TresConeGeometry :args="[1, 1.5, 3]" />
-          <TresMeshToonMaterial color="#82DBC5" />
-        </TresMesh>
-        <TresMesh :position="[0, 4, 0]" cast-shadow>
-          <TresBoxGeometry :args="[1.5, 1.5, 1.5]" />
-          <TresMeshToonMaterial color="#4F4F4F" />
-        </TresMesh>
-        <TresMesh :position="[2, 2, 0]" cast-shadow>
-          <TresSphereGeometry />
-          <TresMeshToonMaterial color="#FBB03B" />
-        </TresMesh>
-      </TresGroup>
-
-      <TresDirectionalLight :position="[0, 2, 4]" :intensity="0.75" cast-shadow />
+      <TresAmbientLight :intensity="0.5" />
+      <TresMesh :position="[0, 2, 0]" cast-shadow>
+        <TresSphereGeometry />
+        <TresMeshToonMaterial color="#FBB03B" />
+      </TresMesh>
+      <TresDirectionalLight :position="[0, 2, 4]" :intensity="1" cast-shadow />
+      <TresAxesHelper />
+      <TresGridHelper :args="[10, 10, 0x444444, 'teal']" />
     </TresScene>
   </TresCanvas>
 </template>
