61025b4382f0d7d85f23e62b5706d684509a4803
alvarosabu <alvaro.saburido@gmail.com>
2023-10-26 17:32:47 +0200

    chore: add vue template compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import glsl from 'vite-plugin-glsl'
+import { templateCompilerOptions } from '@tresjs/core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: tag => (tag.startsWith('Tres') && tag !== 'TresCanvas') || tag === 'primitive',
-        },
-      },
+      ...templateCompilerOptions,
     }),
     glsl(),
   ],
