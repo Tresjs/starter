72e9b8c5b154a113e5db33ee8c2f8675ea344e56
alvarosabu <alvaro.saburido@gmail.com>
2024-07-26 08:10:47 +0200

    chore: delete dummy component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,9 @@
 import { TresCanvas } from '@tresjs/core'
 
 import TheExperience from './components/TheExperience.vue'
-import MyButton from './components/MyButton.vue'
 </script>
 
 <template>
-  <MyButton />
   <TresCanvas clear-color="#82DBC5" window-size>
     <TheExperience />
   </TresCanvas>
